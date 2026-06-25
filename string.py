d6351869d490c9373dfb245658af2236c2038d53
Ghulam Abbas <codewithabbas20222gmail.com>
2026-06-25 16:16:17 +0500

    new code

--- string.py
+++ string.py
@@ -1,4 +1,96 @@
-name="abbas"
-print(name.upper())
-print(name.lower())
-print(len(name))
+# name="abbas"
+# print(name.upper())
+# print(name.lower())
+# print(len(name))
+# print(name[-1])
+
+
+# indexing
+name = "Abbas"
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+
+# print(name[-1])
+# print(name[-2])
+# print(name[-3])
+# print(name[-4])
+# print(name[0])
+
+# word = "Python"
+
+# print(word[0])   # ?
+# print(word[-1])  # ?
+# print(word[3])   # ?
+
+# slicing
+
+# name = "Abbas"
+
+# print(name[0:3])
+
+# name = "Abbas"
+
+# print(name[1:4])
+
+# name = "Abbas"
+
+# print(name[:3])
+
+# name = "Abbas"
+
+# print(name[2:])
+
+# word = "Python"
+
+# print(word[::2])
+
+# word = "Python"
+
+# print(word[::-1])
+
+# name="abbas"
+
+# print(name.upper())
+
+# name="ABBAS"
+
+# print(name.lower())
+
+# name="ghulam abbas"
+
+# print(name.title())
+
+# print(name.capitalize())
+# print(len(name))
+
+# name="mohsin"
+# print(name.replace("m","a"))
+
+
+# name="hassan"
+# print(name.count("s"))
+
+# name = "Abbas"
+
+# print(name.find("a"))
+
+# name = "Abbas"
+
+# print(name.startswith("A"))
+
+# name = "Abbas"
+
+# print(name.endswith("s"))
+
+# name = "  Abbas  "
+
+# print(name.strip())
+
+# text = "Python AI ML"
+
+# print(text.split())

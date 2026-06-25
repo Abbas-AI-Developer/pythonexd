4d6dec6b9ad8b3ed15f2c8482c75cfed18a039b3
Ghulam Abbas <codewithabbas20222gmail.com>
2026-06-25 13:04:12 +0500

    new lines add

--- variable.py
+++ variable.py
@@ -1,3 +1,20 @@
 x=10;
 
 print(x)
+
+name="abbas"
+age=26
+is_male=True
+height=5.8
+
+print(name,age,is_male, height)
+
+user_name="abbas"
+
+# wrong declaration 
+# user-name="ali"
+# 1name
+# class
+# @abbas
+
+

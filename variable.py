5a7516a655402dc8f59b96a8ce34b2ae5ec809ec
Ghulam Abbas <codewithabbas20222gmail.com>
2026-06-25 14:13:34 +0500

    new code

--- variable.py
+++ variable.py
@@ -18,3 +18,46 @@ user_name="abbas"
 # @abbas
 
 
+# print(type(age))
+# print(str(age))
+# num ="100"
+# print(int(num))
+# print(type(num))
+
+# num=int(num)
+
+# print(type(num))
+
+
+# a,b=10,5
+# a,b=b,a
+# print(a,b)
+
+
+# x=[1,2]
+# y=x
+# y.append(3)
+
+# print(x)
+
+
+x=10
+
+def value():
+    x=100
+    print(x)
+
+value()    
+print(x)
+
+x=10
+
+def value():
+     global x
+     x=100
+     print(x)
+
+value()    
+print(x)
+print(id(x))
+

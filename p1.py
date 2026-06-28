f27203ccc08e99296a03831215f2550bde0965fb
Ghulam Abbas <codewithabbas20222gmail.com>
2026-06-28 19:15:49 +0500

    new line

---
+++ p1.py
@@ -0,0 +1,70 @@
+# question1
+
+# name=input("enter your name....")
+# age=int(input("enter your age..."))
+# city=input("enter your city... ")
+
+# print(f"my name is {name} i am {age} from {city}")
+
+# question2
+# name="abbas"
+# print(name.upper())
+# print(name.lower())
+# print(len(name))
+# print(name[0])
+# print(name[4])
+# print(name[::-1])
+
+# # question3
+# numbers=[10,20,30,40]
+# numbers.append(50)
+# print(numbers)
+
+# print(numbers.remove(40))
+# print(numbers)
+# numbers.pop(3)
+# print(numbers)
+# numbers.insert(1,100)
+
+# print(numbers)
+# question4
+# fruits=["Apple","Banana","Mango"]
+
+# for fruit in fruits:
+#        print(fruit)
+
+# question5
+# marks=[10,20,30,40,50]
+
+# print(sum(marks))
+# avrg=sum(marks)/len(marks)
+# print(avrg)
+
+# print(max(marks))
+# print(min(marks))
+
+# question6
+# evens=[]
+# for i in range(1,20):
+#    if i%2==0:
+#     evens.append(i)
+
+# print(evens)
+
+# questions7
+# matrix=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+
+# print(matrix[1][1])
+# print(matrix[2][2])
+# print(matrix[0][0])
+
+# question8
+
+fruits=["apple","mango","orange"]
+
+print("mango" in fruits)
+print("Grapes" in fruits)
